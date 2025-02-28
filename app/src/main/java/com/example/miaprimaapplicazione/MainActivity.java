aed9f3e968751f807e3f82737042cda42c34cc15
st10562 <st10562@iismarconipieralisi.it>
2025-02-28 09:57:32 +0100

    prova di commit

--- app/src/main/java/com/example/miaprimaapplicazione/MainActivity.java
+++ app/src/main/java/com/example/miaprimaapplicazione/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+
         Button button = findViewById(R.id.button);
         Button button2 = findViewById(R.id.Registrazione);
 
